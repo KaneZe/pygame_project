50450ca018771a7755d671562a81c26cd291fcdb
dmitry <KaneZet@yandex.ru>
2020-04-26 18:50:28 +0300

    Add rectangle to our project

--- main.py
+++ main.py
@@ -7,18 +7,27 @@ pygame.init()
 # Задаем количество кадров в секунду для приложения
 FPS = 60
 
+# Задаем необходимые цвета
+LIGHTGREEN = (0, 255, 127)
+
 # Задаем объекты, создаем главное окно
 clock = pygame.time.Clock()
-pygame.display.set_mode((600, 400))
+display = pygame.display.set_mode((600, 400))
 pygame.display.set_caption("My first game")
+
+
+# Создаем фигуры
+pygame.draw.rect(display, LIGHTGREEN, (50, 50, 100, 100))
+pygame.display.update()
 
 
 # Функция для бесконечного цикла и обработчика событий
 def main():
-    # Создаем задержку
-    clock.tick(FPS)
     run = True
     while run:
+        # Создаем задержку
+        clock.tick(FPS)
+        # Обработчик для нажатия на Х
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
