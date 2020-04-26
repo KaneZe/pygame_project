50d2f3531785f7314e8d3edfaaabdec90dbe6cc2
dmitry <KaneZet@yandex.ru>
2020-04-26 15:36:46 +0300

    Created infinity cycle, handlers and FPS

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
 import pygame
 import tkinter
 
+# Инициализация модулей pygame
 pygame.init()
 
-# Создание окна 600х400 пикселей
-pygame.display.set_mode((600, 400))
+# Задаем количество кадров в секунду для приложения
+FPS = 60
 
-# Создание бесконечно цикла
-while 1:
-    for event in pygame.event.get():
-        # При нажатии на Х, выходим из программы
-        if event.type == pygame.QUIT:
-            pygame.quit()
+# Задаем объекты, создаем главное окно
+clock = pygame.time.Clock()
+pygame.display.set_mode((600, 400))
+pygame.display.set_caption("My first game")
+
+
+# Функция для бесконечного цикла и обработчика событий
+def main():
+    # Создаем задержку
+    clock.tick(FPS)
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+
+if __name__ == "__main__":
+    main()
