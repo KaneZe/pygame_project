af5426865cb3423908125dcb7b1be2f97573ce1e
dmitry <KaneZet@yandex.ru>
2020-04-26 15:11:18 +0300

    Create a QUIT event

--- main.py
+++ main.py
@@ -6,3 +6,9 @@ pygame.init()
 # Создание окна 600х400 пикселей
 pygame.display.set_mode((600, 400))
 
+# Создание бесконечно цикла
+while 1:
+    for event in pygame.event.get():
+        # При нажатии на Х, выходим из программы
+        if event.type == pygame.QUIT:
+            pygame.quit()
